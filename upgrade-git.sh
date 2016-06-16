65cc0658b745b3016456b95b72706240e789c424
Cole Kennedy <colek42@gmail.com>
2016-06-15 22:32:31 -0400

    updated upgrade git script

--- upgrade-git.sh
+++ upgrade-git.sh
@@ -1,12 +1,39 @@
 #!/bin/sh
 
 #this script upgrades git
-export GITSRC=$HOME/git
+
 sudo echo "Got Sudo!"
-cd $GITSRC
+export GITSRC="${HOME}/git"
+export PREFIX="/usr/local"
+
+CURRVER=`git --version`
+
+sudo yum groupinstall "Development Tools"
+sudo yum install gettext-devel openssl-devel perl-CPAN perl-devel zlib-devel
+
+
+
+#Git is not already cloned
+if [ ! -d "${GITSRC}/.git" ]; then
+  echo "Cant Find Git Repo"
+  sudo rm -rf $GITSRC
+  cd $HOME
+  git clone git://git.kernel.org/pub/scm/git/git.git
+  cd $GITSRC
+fi
+
+if [ -d "${GITSRC}/.git" ]; then
+  cd $GITSRC
+  echo "Have Git"
+fi
 
 git pull
 make configure
-./configure --prefix=/usr
-make all
+./configure --prefix=/usr/local
 sudo make install
+
+NEWVER=`git --version`
+
+echo "Done GIT Upgraded from ${CURRVER} to ${NEWVER}"
+
+
